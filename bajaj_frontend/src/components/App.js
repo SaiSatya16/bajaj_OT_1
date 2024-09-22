691fde7555db52a2b8ab3efd852cf5aa440b5597
jj1039 <jj1039@srmist.edu.in>
2024-09-22 18:08:20 +0530

    name added to frontend landing page

--- bajaj_frontend/src/components/App.js
+++ bajaj_frontend/src/components/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Bajaj Finserv Health Dev Challenge</h1>
+      <h3>Jonnalagadda Sai Satya Narayana - RA2111003011154</h3>
       <InputForm />
     </div>
   );
