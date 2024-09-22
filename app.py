ea1ba2035c32d0c926f33f36327dab14dbbb4292
jj1039 <jj1039@srmist.edu.in>
2024-09-22 16:50:30 +0530

    updated requirements

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from api import *
+#import cors
+from flask_restful import Api
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 api.init_app(app)
 app.app_context().push()
-
+CORS(app)
 
 
 if __name__ == "__main__":
